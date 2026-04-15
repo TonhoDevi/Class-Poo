ed45594234f24735093db1802767879a8c837157
Antonio Henrique <antoniohenriquebn@hotmail.com>
2026-04-14 21:55:19 -0300

    main change

--- aula_poo/src/controllers/App.java
+++ aula_poo/src/controllers/App.java
@@ -2,9 +2,11 @@ package controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 
 import estoques.ProdutoEstoque;
+import menus.Inicial;
 import models.Produto;
 import services.ProdutoService;
 
@@ -12,22 +14,12 @@ public class App {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		// ProdutoService com função generica = createProd
-		ProdutoEstoque bancoTemp = new ProdutoEstoque();
+		List<Produto> armazem = new ArrayList<>();	
 		ProdutoService funcProd = new ProdutoService();
-		List<Produto> armazem = bancoTemp.uploadEstoque();
-		
-		System.out.println("O numero total de produtos é: " + armazem.size());
-		System.out.println("Listando todos os produtos:");
-		System.out.println(funcProd.listAll(armazem));
-		
-		
-		/*
-		int escolhido;
-		
-		escolhido = scan.nextInt();
-		double resultado = funcProd.valorPorProduto(armazem.get(escolhido));
-		System.out.println("R$" + resultado);*/
+		ProdutoEstoque bancoDados = new ProdutoEstoque();
+		armazem = bancoDados.uploadEstoque();
+		Inicial menuInicial = new Inicial();
+		menuInicial.showMenu(armazem);
 		
 	}
 
