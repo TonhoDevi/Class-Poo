7d55ca7d9a1f141bc812487d3f73046f3b8b923c
Antonio Henrique <antoniohenriquebn@hotmail.com>
2026-04-14 21:56:06 -0300

    Update Service

--- aula_poo/src/services/ProdutoService.java
+++ aula_poo/src/services/ProdutoService.java
@@ -2,15 +2,18 @@ package services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import models.Produto;
 
 public class ProdutoService {
+
 	public static double valorPorProduto(Produto prod) {
 		double valor;
 		valor = prod.getValor() * prod.getUnidades();
 		return valor;
 	}
+
 	public static Produto createProd(String nomeProd, double valorProd, int unidadesProd, String descricaoProd) {
 		Produto prod = new Produto();
 		prod.setNome(nomeProd);
@@ -20,17 +23,66 @@ public class ProdutoService {
 		return prod;
 	}
 	
-	public static List<String> listAll(List<Produto> armazemProd){
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	public static List<String> listAll(List<Produto> armazemProd) {
 		List<String> nomesProd = new ArrayList<>();
-		for(int i = 0; i<armazemProd.size();i++) {
+
+		for (int i = 0; i < armazemProd.size(); i++) {
 			nomesProd.add(armazemProd.get(i).getNome());
 		}
 		return nomesProd;
 	}
+
+	public static void deleteProd(List<Produto> armazemProd, int idP) {
+
+		for (int i = 0; i < armazemProd.size(); i++) {
+
+			if (armazemProd.get(i).getID() == idP) {
+
+				armazemProd.remove(i);
+
+				return;
+			}
+
+		}
+	}
+	
+	public static Optional<Produto> findById(List<Produto> armazemProd, int idP) {
+		
+		for(int i = 0; i<armazemProd.size();i++) {
+			if(armazemProd.get(i).getID() == idP) {
+				return Optional.of(armazemProd.get(i));
+			}
+			
+		}
+		return Optional.empty();
+		
+	}
 	
 	
 	
-	
-	
-	
+
 }
